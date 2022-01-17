364b68bf04e5f991221e32ad08d0a9514473d812
asrakt <ashraktamin678@gmail.com>
2022-01-17 11:28:32 +0200

    push

---
+++ database/migrations/2022_01_13_154157_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->string('image')->nullable();
+            $table->float('price')->unsigned();
+            $table->unsignedInteger('quantity')->default(0);
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
